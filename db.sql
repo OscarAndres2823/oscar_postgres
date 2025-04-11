5a9a258b1a32bb29e741d5df84222968da65a43d
OscarAndres2823 <oscarandresortiz17@gmail.com>
2025-04-11 08:37:20 -0500

    segundo commit

--- db.sql
+++ db.sql
@@ -1,68 +1,122 @@
 CREATE DATABASE techzone;
+\c techzone -- Conectarse a la base de datos (PostgreSQL)
+
+-- Tabla Cliente
 CREATE TABLE cliente(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50),
     email VARCHAR(50),
-    numero_telefono NUMBER 
+    numero_telefono NUMERIC
 );
+
+-- Tabla Proveedor
 CREATE TABLE proveedor(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50),
     email VARCHAR(50),
-    numero_telefono NUMBER 
+    numero_telefono NUMERIC
 );
+
+-- Tabla Producto
 CREATE TABLE producto(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     categoria VARCHAR(50),
-    precio NUMBER,
-    stock VARCHAR(50),
+    precio NUMERIC,
+    stock INT,
     disponibilidad VARCHAR(50),
-    FOREIGN KEY id_proveedor REFERENCES proveedor(id_proveedor)
+    id_proveedor INT,
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id)
 );
+
+-- Tabla Ventas
 CREATE TABLE ventas(
     id INT PRIMARY KEY,
-    FOREIGN KEY id_producto REFERENCES productos(id_producto),
-    cantidad VARCHAR(50)
-    FOREIGN KEY id_cliente REFERENCES cliente(id_cliente)
+    id_producto INT,
+    id_cliente INT,
+    cantidad INT,
+    FOREIGN KEY (id_producto) REFERENCES producto(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
+
+-- Tabla País
 CREATE TABLE pais(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     serial VARCHAR(25)
 );
+
+-- Tabla Departamento
 CREATE TABLE departamento(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
-    FOREIGN KEY id_pais REFERENCES pais(id_pais)
+    id_pais INT,
+    FOREIGN KEY (id_pais) REFERENCES pais(id)
 );
+
+-- Tabla Ciudad
 CREATE TABLE ciudad(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
-    FOREIGN KEY id_departamento REFERENCES departamento(id_departamento)
+    id_departamento INT,
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id)
 );
+
+-- Tabla Barrio
 CREATE TABLE barrio(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
-    FOREIGN KEY id_ciudad REFERENCES ciudad(id_ciudad)
+    id_ciudad INT,
+    FOREIGN KEY (id_ciudad) REFERENCES ciudad(id)
 );
+
+-- Tabla Dirección
 CREATE TABLE direccion( 
     id INT PRIMARY KEY,
-    lugar NUMBER,
-    FOREIGN KEY id_barrio REFERENCES barrio(id_barrio)
+    lugar VARCHAR(50),
+    id_barrio INT,
+    FOREIGN KEY (id_barrio) REFERENCES barrio(id)
 );
+
+-- Tabla Empleados
 CREATE TABLE empleados(
     id INT PRIMARY KEY,
     nombre VARCHAR(50),
     apellido VARCHAR(50),
     email VARCHAR(50),
-    telefono NUMBER
+    telefono NUMERIC
 );
+
+-- Tabla Empresa
 CREATE TABLE empresa(
     id INT PRIMARY KEY,
-    FOREIGN KEY id_direccion REFERENCES direccion(id_direccion),
-    FOREIGN KEY id_empleado REFERENCES empleado(id_empleado),
-    FOREIGN KEY id_proveedor REFERENCES proveedor(id_proveedor)
+    id_direccion INT,
+    id_empleado INT,
+    id_proveedor INT,
+    id_cliente INT,
+    FOREIGN KEY (id_direccion) REFERENCES direccion(id),
+    FOREIGN KEY (id_empleado) REFERENCES empleados(id),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
+);
+
+-- Tabla Transacciones
+CREATE TABLE transacciones(
+    id INT PRIMARY KEY,
+    id_producto INT,
+    id_cliente INT,
+    FOREIGN KEY (id_producto) REFERENCES producto(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
+);
+
+-- Tabla Precio
+CREATE TABLE precio(
+    id INT PRIMARY KEY,
+    valor NUMERIC,
+    id_producto INT,
+    id_proveedor INT,
+    FOREIGN KEY (id_producto) REFERENCES producto(id),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id)
 );
---
+++ insert.sql
@@ -0,0 +1,27 @@
+--CLIENTE 
+INSERT INTO cliente (id, nombre, apellido, email, numero_telefono) VALUES 
+(1, 'pepe', 'ramirez', 'pepe@gamil.com', '3167687678'),
+(2, 'maria', 'ortiz', 'maria@gmail.com', '3124567896'),
+(3, 'oscar', 'toloza', 'oscar@gmail.com', '3161408809'),
+(4, 'thiago', 'ortiz', 'thiago@gmail.com', '3219627892'),
+(5, 'andres', 'aceros', 'andres@gmail.com', '3157593256'),
+(6, 'Nathan', 'Martin', 'hamiltondavid@yahoo.com', 7559634350)
+(7, 'Linda', 'Franklin', 'derekrivera@roberts.com', 8750111217)
+ (8, 'Jordan', 'Miranda', 'melissajohnson@fields-taylor.com', 5689644699)
+ (9, 'Andrea', 'Jenkins', 'angela28@hoffman-lucas.com', 6057667031)
+ (10, 'Lisa', 'Hunter', 'kyle89@yahoo.com', 2523940965)
+ (11, 'Nancy', 'Pace', 'april46@chavez.biz', 3728455542)
+  (12, 'Cory', 'Wallace', 'nancy21@gmail.com', 2173275924)
+ (13, 'Megan', 'Mora', 'myersjody@robinson-paul.com', 5123445087)
+(14, 'Vanessa', 'Weiss', 'brittanyporter@reid.com', 5387501517)
+ (15, 'Darrell', 'Ewing', 'johnwagner@gmail.com', 3019316898);
+
+--PROVEEDOR
+INSERT INTO proveedor (id, nombre, apellido, email, numero_telefono) VALUES 
+(1, 'juan', 'ramirez', 'juan@gamil.com', '3167687987'),
+(2, 'marta', 'almeida', 'marta@gmail.com', '3124567987'),
+(3, 'marcos', 'toloza', 'marcos@gmail.com', '3161408876'),
+(4, 'cruz', 'aceros', 'cruz@gmail.com', '3245327892'),
+(5, 'dario', 'rosales', 'dario@gmail.com', '3157593578');
+
+  
